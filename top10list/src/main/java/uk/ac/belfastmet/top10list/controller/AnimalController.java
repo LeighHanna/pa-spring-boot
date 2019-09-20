b44a92f5e3bb865300894db8d0d083ad9d4905d4
LeighHanna <leighhanna1996@gmail.com>
2019-09-20 11:55:01 +0100

    20/09/2019

--- top10list/src/main/java/uk/ac/belfastmet/top10list/controller/AnimalController.java
+++ top10list/src/main/java/uk/ac/belfastmet/top10list/controller/AnimalController.java
@@ -1,19 +1,28 @@
 package uk.ac.belfastmet.top10list.controller;
 
+
+
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import uk.ac.belfastmet.top10list.service.AnimalService;
-
+//first hit - the doorman : in or out requests 
 @Controller
 @RequestMapping
 public class AnimalController {
 	
-	@GetMapping()
+	Logger logger = LoggerFactory.getLogger(AnimalController.class);
+	
+	@RequestMapping(value="", method = RequestMethod.GET)
 	public String homepage(Model model) {
-		
+		logger.info("Here homepage");
 		model.addAttribute("pageTitle", "HomePageAnimals");
 		model.addAttribute("numberOfAnimals", "Top 10 fastest animals");
 		AnimalService animalService = new AnimalService();
