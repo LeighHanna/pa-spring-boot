2129cee0e35bb6972b25e57994664f327bd51b8e
LeighHanna <leighhanna1996@gmail.com>
2019-09-23 12:02:00 +0100

    quick update

--- ToDo/src/main/java/uk/ac/belfastmet/todo/controller/TaskController.java
+++ ToDo/src/main/java/uk/ac/belfastmet/todo/controller/TaskController.java
@@ -24,12 +24,72 @@ public class TaskController {
 	public String homepage(Model model) {
 		//text assignment for page
 		logger.info("Homepage loaded");
-		model.addAttribute("pageTitle", "HomePage");
+		model.addAttribute("pageTitle", "Task Home Page");
 		model.addAttribute("taskpgDescription", "A complete list of tasks");
 		//getting task info for table population
 		TaskService taskService = new TaskService();
 		model.addAttribute("tasks", taskService.getTasks());
 		logger.info("Tasks method run");
+		
+		
+		return "index";
+	}
+	
+	/**
+	 * This method is what happens for the completed webpage
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="/completed", method = RequestMethod.GET)
+	public String completed(Model model) {
+		//text assignment for page
+		logger.info("completedPage loaded");
+		model.addAttribute("pageTitle", "Completed Page");
+		model.addAttribute("taskpgDescription", "A complete list of completed tasks");
+		//getting task info for table population
+		TaskService taskService = new TaskService();
+		model.addAttribute("tasks", taskService.getTasksCompleted());
+		logger.info("Tasks completed method run");
+		
+		
+		return "index";
+	}
+	
+	/**
+	 * This method is what happens for the todo webpage
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="/todo", method = RequestMethod.GET)
+	public String todo(Model model) {
+		//text assignment for page
+		logger.info("completedPage loaded");
+		model.addAttribute("pageTitle", "ToDo Page");
+		model.addAttribute("taskpgDescription", "A complete list of tasks still todo");
+		//getting task info for table population
+		TaskService taskService = new TaskService();
+		model.addAttribute("tasks", taskService.getTasksToDo());
+		logger.info("Tasks todo method run");
+		
+		
+		return "index";
+	}
+	
+	/**
+	 * This method is what happens for the alltasks webpage
+	 * @param model
+	 * @return
+	 */
+	@RequestMapping(value="/alltasks", method = RequestMethod.GET)
+	public String alltasks(Model model) {
+		//text assignment for page
+		logger.info("completedPage loaded");
+		model.addAttribute("pageTitle", "All Tasks Page");
+		model.addAttribute("taskpgDescription", "A complete list of tasks");
+		//getting task info for table population
+		TaskService taskService = new TaskService();
+		model.addAttribute("tasks", taskService.getTasks());
+		logger.info("Tasks todo method run");
 		
 		
 		return "index";
--- ToDo/src/main/java/uk/ac/belfastmet/todo/service/TaskService.java
+++ ToDo/src/main/java/uk/ac/belfastmet/todo/service/TaskService.java
@@ -11,14 +11,75 @@ import uk.ac.belfastmet.todo.domain.Task;
 public class TaskService {
 	private ArrayList<Task> tasks;
 	
-	
+	/**
+	 * method to create and receive a array list of tasks and their data for tables
+	 * @return
+	 */
 	public ArrayList<Task> getTasks()
 	{
 		 this.tasks = new ArrayList<Task>();
 		 
+		 //creating and adding all tasks
 		 this.tasks.add(new Task("Go shopping", "Go to asda get milk and bread", "24/09/2019", false, "Leigh Hanna", "low"));
+		 this.tasks.add(new Task("MTA test", "go to titanic at 17:20", "29/09/2019", false, "Leigh Hanna", "high"));
+		 this.tasks.add(new Task("Go shopping", "Go to asda get milk and bread", "19/09/2019", true, "Leigh Hanna", "low"));
+		 this.tasks.add(new Task("Visit bob", "good old bob", "24/10/2019", false, "Leigh Hanna", "low"));
+		 this.tasks.add(new Task("Visit bob", "Gain revenge on bob", "25/10/2019", false, "Leigh Hanna", "Medium"));
+		 this.tasks.add(new Task("hide bob", "bury the evidence", "26/10/2019", false, "Not Leigh Hanna", "High"));
+		 this.tasks.add(new Task("meet friends", "establish the alibi", "26/10/2019", false, "Not Leigh Hanna", "High"));
+		 this.tasks.add(new Task("study java", "make a list program", "27/10/2019", false, "Leigh Hanna", "low"));
+		 this.tasks.add(new Task("Go shopping", "Go to asda get milk and bread", "28/09/2019", false, "Leigh Hanna", "low"));
+		 this.tasks.add(new Task("Go to jail", "they caught me", "01/11/2019", false, "Leigh Hanna", "High"));
 		 
-		 
+		 //returning the populated list
 		 return this.tasks;
+	}
+	
+	/**
+	 * method to create and receive a array list of tasks and their data for tables only completed
+	 * @return
+	 */
+	public ArrayList<Task> getTasksCompleted()
+	{
+		 ArrayList<Task> tasksNew = new ArrayList<Task>();
+		 getTasks();
+		 
+		 //creating and adding all tasks
+		 for(Task task : tasks)
+		 {
+			 if(task.isComplete())
+			 {
+				 tasksNew.add(task);
+			 }
+		 }
+		 
+		 //returning the populated list
+		 return tasksNew;
+	}
+	
+	/**
+	 * method to create and receive a array list of tasks and their data for tables only non completed
+	 * @return
+	 */
+	public ArrayList<Task> getTasksToDo()
+	{
+		 ArrayList<Task> tasksNew = new ArrayList<Task>();
+		 getTasks();
+		 
+		 //creating and adding all tasks
+		 for(Task task : tasks)
+		 {
+			 if(task.isComplete())
+			 {
+				 
+			 }
+			 else
+			 {
+				 tasksNew.add(task);
+			 }
+		 }
+		 
+		 //returning the populated list
+		 return tasksNew;
 	}
 }
